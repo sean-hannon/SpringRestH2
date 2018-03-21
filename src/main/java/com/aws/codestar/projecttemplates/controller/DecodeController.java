d87746aa48858e3e03afb00f551157b61dc49662
Sean Hannon <sean.hannon@travelportdigital.com>
2018-03-21 10:58:06 +0000

    Added endpoint and return not implemented error

--- src/main/java/com/aws/codestar/projecttemplates/controller/DecodeController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/DecodeController.java
@@ -1,4 +1,16 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/decode", method = RequestMethod.GET, produces = "application/json")
 public class DecodeController {
+
+  public ResponseEntity decode(){
+    return new ResponseEntity(null, HttpStatus.NOT_IMPLEMENTED);
+  }
 }
--- src/main/java/com/aws/codestar/projecttemplates/controller/EncodeController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/EncodeController.java
@@ -1,4 +1,16 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/encode", method = RequestMethod.POST, produces = "application/json")
 public class EncodeController {
+
+  public ResponseEntity encode(){
+    return new ResponseEntity(null, HttpStatus.NOT_IMPLEMENTED);
+  }
 }
